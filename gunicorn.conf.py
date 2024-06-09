a961cc139c4318083e275d503fa88e56c7788d13
Aman Singh <amansinghjiit@gmail.com>
2024-06-09 16:31:32 +0530

    config files gunicorn

---
+++ gunicorn.conf.py
@@ -0,0 +1,23 @@
+import logging
+from gunicorn.glogging import Logger
+
+loglevel = 'debug'
+errorlog = '-'
+accesslog = '-'
+capture_output = True
+worker_class = 'uvicorn.workers.UvicornWorker'
+workers = 2
+timeout = 60
+graceful_timeout = 60
+worker_connections = 500
+preload_app = True
+
+class CustomLogger(Logger):
+    def setup(self, cfg):
+        super().setup(cfg)
+        self.error_log.setLevel(logging.DEBUG)
+        self.access_log.setLevel(logging.INFO)
+        self.error_log.propagate = False
+        self.access_log.propagate = False
+
+logger_class = CustomLogger
