8a39613919dab229bc907083462f8ff515fab7c0
Aman Singh <amansinghjiit@gmail.com>
2024-06-12 18:31:50 +0530

    build.sh modified

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py flush --no-input
-python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --run-syncdb
 
 python manage.py shell <<EOF
 from django.contrib.auth.models import User
