468eb36a694fbb01409fe370b064738fea4905ff
Aman Singh <amansinghjiit@gmail.com>
2024-06-12 19:39:31 +0530

    build.sh updated

--- build.sh
+++ build.sh
@@ -7,4 +7,16 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate --run-syncdb
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('amannick2', 'amannick2@gmail.com', 'kibtypen')" | python manage.py shell
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+
+username = 'amannick2'
+email = 'amannick2@gmail.com'
+password = 'kibtypen'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created successfully.")
+else:
+    print("Superuser already exists. Skipping creation.")
+EOF
