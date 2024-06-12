c67aba62625899c4c1f16043cd134c599ff4b7ec
Aman Singh <amansinghjiit@gmail.com>
2024-06-12 17:41:58 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py migrate --run-syncdb
+python manage.py migrate
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('amannick2', 'amannick2@gmail.com', 'kibtypen')" | python manage.py shell
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='amannick2').exists():
+    User.objects.create_superuser('amannick2', 'amannick2@gmail.com', 'kibtypen')
+EOF
