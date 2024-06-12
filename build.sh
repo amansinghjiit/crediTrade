c6c17c3f2a50ffacf876cae0aaf7fd7e8ed9719b
Aman Singh <amansinghjiit@gmail.com>
2024-06-12 18:05:51 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
 
 python manage.py shell <<EOF
