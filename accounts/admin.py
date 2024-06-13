75818e772320774fe40ee95f030406f53f0e07c3
Aman Singh <amansinghjiit@gmail.com>
2024-06-13 20:47:39 +0530

    admin.py updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('name', 'whatsapp_number', 'phonepe_name', 'phonepe_number','amount_due')
+    list_display = ('name', 'whatsapp_number', 'phonepe_name', 'phonepe_number','amount_due','last_login_ip')
     search_fields = ('name', 'whatasapp_number')
 
 admin.site.register(UserProfile, UserProfileAdmin)
