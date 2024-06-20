479ef7a9168405a536c8dc730550db9a66d40523
Aman Singh <amansinghjiit@gmail.com>
2024-06-20 19:35:50 +0530

    fixed typo mistake

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,6 @@ from .models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('name', 'whatsapp_number', 'phonepe_name', 'phonepe_number','amount_due','last_login_ip')
-    search_fields = ('name', 'whatasapp_number')
+    search_fields = ('name', 'whatsapp_number')
 
 admin.site.register(UserProfile, UserProfileAdmin)
