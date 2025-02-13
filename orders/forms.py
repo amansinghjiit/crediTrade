4639a956f3ec98528b993281970debc88bf01de5
Aman Singh <amansinghjiit@gmail.com>
2025-02-13 10:28:47 +0530

    fixed model issue

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import PendingOrder
+from .models_fetching import get_models
 
 class PendingOrderForm(forms.ModelForm):
+    model = forms.ChoiceField(choices=[], required=False, widget=forms.Select(attrs={'class': 'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        model_choices = [('', 'Model')] + [(m, m) for m in get_models()]
+        if self.instance and self.instance.pk and self.instance.model:
+            if self.instance.model not in dict(model_choices):
+                model_choices.append((self.instance.model, self.instance.model))
+        self.fields['model'].choices = model_choices
+
     class Meta:
         model = PendingOrder
         fields = ['name', 'tracking', 'otp', 'obd', 'model', 'pin']
@@ -10,6 +21,5 @@ class PendingOrderForm(forms.ModelForm):
             'tracking': forms.TextInput(attrs={'placeholder': 'Tracking'}),
             'otp': forms.TextInput(attrs={'placeholder': 'OTP'}),
             'obd': forms.TextInput(attrs={'placeholder': 'Last 4 digits of phone no.'}),
-            'model': forms.Select(attrs={'class': 'form-control'}),  # No need to set choices here
             'pin': forms.Select(attrs={'class': 'form-control'}),
         }
