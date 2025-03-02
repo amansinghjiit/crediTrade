ddbc5a61d11491f8fcac24433be5d1917a5115a5
Aman Singh <amansinghjiit@gmail.com>
2025-03-02 14:51:45 +0530

    updated pwa app

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -2,7 +2,18 @@ const CACHE_NAME = 'creditrade-cache-v1';
 const urlsToCache = [
   '/',
   '/static/css/styles.css',
-  '/static/js/scripts.js'    
+  '/static/css/orders.css',
+  '/static/css/analytics.css',
+  '/static/js/scripts.js',
+  '/static/images/3_dots.svg',
+  '/static/images/copy.svg',
+  '/static/images/ddfavicon.png',
+  '/static/images/delete.svg',
+  '/static/images/edit.svg',
+  '/static/images/eye.svg',
+  '/static/images/fbgroup.png',
+  '/static/images/logo.png',
+  '/static/images/upload.png'
 ];
 
 self.addEventListener('install', event => {
