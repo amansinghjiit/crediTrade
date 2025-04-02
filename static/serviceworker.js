0695e1203e763a08501dc511d8aef96c4e699d2c
Aman Singh <amansinghjiit@gmail.com>
2025-04-02 09:58:04 +0530

    updated pwa animation

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -4,7 +4,6 @@ const urlsToCache = [
   '/static/css/styles.css',
   '/static/css/orders.css',
   '/static/css/analytics.css',
-  '/static/js/scripts.js',
   '/static/images/3_dots.svg',
   '/static/images/copy.svg',
   '/static/images/ddfavicon.png',
@@ -14,11 +13,17 @@ const urlsToCache = [
   '/static/images/fbgroup.png',
   '/static/images/logo.png',
   '/static/images/upload.png',
+  '/static/js/scripts.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css',
   'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.js',
 ];
 
 self.addEventListener('install', event => {
